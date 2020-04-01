5dd207a8b584c72fff8ce128fc0d33ee7774273d
joaohag3d <61989665+joaohag3d@users.noreply.github.com>
2020-04-01 16:46:34 -0300

    Add files via upload

--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -18,56 +18,56 @@ export class ListaProdutosComponent implements OnInit {
 
       {
         id: 1,
-        urlImagem: 'https://image.myanimelist.net/ui/5LYzTBVoS196gvYvw3zjwFvByhHpUzCHIZyTAN5T0Jc',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS',
+        urlImagem: 'https://cdn6.bigcommerce.com/s-0kvv9/products/12158/images/58675/yugioh-zexal-photon-shockwave-single-card-ultra-rare-phsw-en011-galaxy-eyes-photon-dragon-19__77683.1461128232.500.750.jpg?c=2',
+        titulo: 'Galaxy-Eyes Photon Dragon',
+        descricaoCurta: 'Dragon/Effect Monster',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: 'Preço: R$10.00',
       },
 
       {
         id: 2,
         urlImagem: 'https://www.lmcorp.com.br/arquivos/in/yugioh_bkp/cd/708/9336.jpg',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS',
+        titulo: 'WitchCrafter Madame Verre',
+        descricaoCurta: 'Spellcaster/Monster Effect',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: 'Preço: R$40,00',
       },
 
       {
         id: 3,
-        urlImagem: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e5cdeafc-dfb9-449a-b254-0a0f947dfbde/dd20jl9-e90572e4-5022-4efa-b5e5-b8bcc5403ed4.jpg/v1/fill/w_750,h_750,q_75,strp/witchcraft_golem_arles_by_batmed_dd20jl9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzUwIiwicGF0aCI6IlwvZlwvZTVjZGVhZmMtZGZiOS00NDlhLWIyNTQtMGEwZjk0N2RmYmRlXC9kZDIwamw5LWU5MDU3MmU0LTUwMjItNGVmYS1iNWU1LWI4YmNjNTQwM2VkNC5qcGciLCJ3aWR0aCI6Ijw9NzUwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.LyX6KzuWC3tPIlgYt28TBprj9TB0QYiBTFl1ADERjlo',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS',
+        urlImagem: 'https://ms.yugipedia.com//thumb/6/61/BottomlessTrapHole-YS18-EN-C-1E.png/300px-BottomlessTrapHole-YS18-EN-C-1E.png',
+        titulo: 'Botomless Trap Hole',
+        descricaoCurta: 'Trap Card',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: 'Preço: R$5.00',
       },
 
       {
         id: 4,
-        urlImagem: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80460507_112016620164150_3740068078483894556_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=106&_nc_ohc=jzE9a6f3D8wAX-UI1ja&oh=835f2d49ac7ae455e04643ce534b223d&oe=5E8549BB',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS.',
+        urlImagem: 'https://ms.yugipedia.com//thumb/0/0c/DarkHole-OP12-EN-C-UE.png/300px-DarkHole-OP12-EN-C-UE.png',
+        titulo: 'Dark Hole',
+        descricaoCurta: 'Spell Card.',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: 'Preço: R$0.60',
       },
 
       {
         id: 5,
-        urlImagem: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0f25432a-eb65-4731-9a59-0c8fe29ae963/dd2emyc-022efff1-2a37-47ac-aafb-766336374977.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBmMjU0MzJhLWViNjUtNDczMS05YTU5LTBjOGZlMjlhZTk2M1wvZGQyZW15Yy0wMjJlZmZmMS0yYTM3LTQ3YWMtYWFmYi03NjYzMzYzNzQ5NzcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1KDFukveeLWt9aB35F9YnU2W2kd7l6M1rjwpEs2rw5k',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS',
+        urlImagem: 'https://ms.yugipedia.com//thumb/2/2c/SalamangreatMiragestallio-SDSB-EN-UR-1E.png/300px-SalamangreatMiragestallio-SDSB-EN-UR-1E.png',
+        titulo: 'Salamangreat Miragestallio',
+        descricaoCurta: 'Cyberse/Effect Monster/XYZ',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: ' Preço: R$2.00',
       },
 
       {
         id: 6,
-        urlImagem: 'https://www.mtgcards.com.br/image/cache/data/0%20-%20ASAST/Witchcrafter%20Creation-500x500.jpg',
-        titulo: 'TESTE SEM CSS',
-        descricaoCurta: 'TESTE SEM CSS',
+        urlImagem: 'https://www.lmcorp.com.br/arquivos/in/yugioh_bkp/cd/251/80.png',
+        titulo: 'Exodia The Forbidden One',
+        descricaoCurta: 'OBLITERAR',
         descricaoCompleta: 'TESTE SEM CSS',
-        valor: 'TESTE SEM CSS',
+        valor: 'R$10000000000.0',
       }
 
     ];
